49ccfec1d01104c82fb182aa23d8b088fe44540b
Brooke Bennett <bennetb2@oregonstate.edu>
2019-09-09 15:01:42 -0700

    Changed if statement

--- spatial-crop-yield.py
+++ spatial-crop-yield.py
@@ -7,7 +7,10 @@ from shapely.geometry import Point, Polygon
 
 # Step 1: SPATIALLY FILTER: Data filtered based on defined polygon for the field coordinates
 def filter_field_boundaries(cfg):
+    # Defining the corners of the field using latitude/longitude coordinates to create a polygon
+    # Geopandas library accounts for geometry of the polygons and land surfaces
     fields = geopandas.GeoSeries({
+    # Fields 1 and 2 call on the manually entered points in the config file
     'field_1': Polygon([(cfg['field_boundaries'][0]['NW_corner']['latitude'], cfg['field_boundaries'][0]['NW_corner']['longitude']),
                         (cfg['field_boundaries'][0]['NE_corner']['latitude'], cfg['field_boundaries'][0]['NE_corner']['longitude']),
                         (cfg['field_boundaries'][0]['SE_corner']['latitude'], cfg['field_boundaries'][0]['SE_corner']['longitude']),
@@ -22,7 +25,8 @@ def filter_field_boundaries(cfg):
     for index, row in df.iterrows():
         try:
             point = Point(float(row['lat']), float(row['long']))
-            if fields.intersects(point)['field_1'] or fields.intersects(point)['field_2']:
+            # if the point is in any defined field, keep the value
+            if any(fields.intersects(point)):
                 filtered_points.append({'latitude': row['lat'], 'longitude':row['long'], 'weight':row['weight']})
         except ValueError:
             print ("Not a float")
@@ -35,8 +39,8 @@ def main():
         filtered_points = filter_field_boundaries(cfg)
 
 
-        # Output_df = pd.DataFrame(data=filtered_points)
-        # Output_df.to_csv('test.csv', index=False)
+        Output_df = pd.DataFrame(data=filtered_points)
+        Output_df.to_csv('test.csv', index=False)
 
 
 if __name__ == '__main__':
