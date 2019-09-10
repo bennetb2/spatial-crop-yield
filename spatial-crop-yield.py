8d24200d87b02980f5005808dfe6db2a74534673
Brooke Bennett <bennetb2@oregonstate.edu>
2019-09-10 09:20:50 -0700

    Generalized polygons for fields

--- spatial-crop-yield.py
+++ spatial-crop-yield.py
@@ -5,32 +5,33 @@ import pandas as pd
 import geopandas
 from shapely.geometry import Point, Polygon
 
-# Step 1: SPATIALLY FILTER: Data filtered based on defined polygon for the field coordinates
+# Step 1: SPATIALLY FILTER: Data filtered based on the field coordinates
 def filter_field_boundaries(cfg):
-    # Defining the corners of the field using latitude/longitude coordinates to create a polygon
+    # Creating a polygon for each field in config
+    field_corners = {}
+    for field in cfg['field_boundaries']:
+        field_corners[field['name']] =  Polygon(
+                            [(field['NW_corner']['latitude'],field['NW_corner']['longitude']),
+                            (field['NE_corner']['latitude'], field['NE_corner']['longitude']),
+                            (field['SE_corner']['latitude'], field['SE_corner']['longitude']),
+                            (field['SW_corner']['latitude'], field['SW_corner']['longitude'])])
     # Geopandas library accounts for geometry of the polygons and land surfaces
-    fields = geopandas.GeoSeries({
-    # Fields 1 and 2 call on the manually entered points in the config file
-    'field_1': Polygon([(cfg['field_boundaries'][0]['NW_corner']['latitude'], cfg['field_boundaries'][0]['NW_corner']['longitude']),
-                        (cfg['field_boundaries'][0]['NE_corner']['latitude'], cfg['field_boundaries'][0]['NE_corner']['longitude']),
-                        (cfg['field_boundaries'][0]['SE_corner']['latitude'], cfg['field_boundaries'][0]['SE_corner']['longitude']),
-                        (cfg['field_boundaries'][0]['SW_corner']['latitude'], cfg['field_boundaries'][0]['SW_corner']['longitude'])]),
-    'field_2': Polygon([(cfg['field_boundaries'][1]['NW_corner']['latitude'], cfg['field_boundaries'][1]['NW_corner']['longitude']),
-                        (cfg['field_boundaries'][1]['NE_corner']['latitude'], cfg['field_boundaries'][1]['NE_corner']['longitude']),
-                        (cfg['field_boundaries'][1]['SE_corner']['latitude'], cfg['field_boundaries'][1]['SE_corner']['longitude']),
-                        (cfg['field_boundaries'][1]['SW_corner']['latitude'], cfg['field_boundaries'][1]['SW_corner']['longitude'])]),
-    })
+    fields = geopandas.GeoSeries(field_corners)
     df = pd.read_csv(cfg['csv_filepath'])
+    # Storing all points located within the field polygons
     filtered_points = []
     for index, row in df.iterrows():
         try:
             point = Point(float(row['lat']), float(row['long']))
-            # if the point is in any defined field, keep the value
+            # If the point is in any defined field, keep the value
             if any(fields.intersects(point)):
+                # Append statement creates an object including the lat/long and weight for included points
                 filtered_points.append({'latitude': row['lat'], 'longitude':row['long'], 'weight':row['weight']})
-        except ValueError:
-            print ("Not a float")
+        except:
+            pass
     return filtered_points
+
+# Step 2: DELTA WEIGHT: Steps through data using a spatial increment to calculate change in weight
 
 
 def main():
@@ -38,7 +39,7 @@ def main():
         cfg = json.load(json_data_file)
         filtered_points = filter_field_boundaries(cfg)
 
-
+        # Creating an output csv after Step 1 to check progress
         Output_df = pd.DataFrame(data=filtered_points)
         Output_df.to_csv('test.csv', index=False)
 
