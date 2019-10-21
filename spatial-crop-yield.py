58db77f885a099d221c83d532ab30e87c9bc4d3b
Ian Wilson <wilsonia@oregonstate.edu>
2019-10-21 11:31:28 -0700

    break out creation of region in helper function

--- spatial-crop-yield.py
+++ spatial-crop-yield.py
@@ -23,30 +23,51 @@ def create_field_polygons(cfg):
     return field_polygons
 
 
-def remove_points_not_in_fields(cfg, filename, field_polygons):
+def load_file(cfg, filename, field_polygons):
     csv_directory = cfg['csv_directory']
     df = pd.read_csv(f'{csv_directory}/{filename}', encoding = "ISO-8859-1")
 
     # Initialize lists for filtered points categorized by field name
-    filtered_points = {}
+    points = {}
     for field in cfg['field_boundaries']:
-        filtered_points[field['name']] = []
+        points[field['name']] = []
 
     for index, row in df.iterrows():
         try:
             point = Point(float(row['lat']), float(row['long']))
-            for field_name in filtered_points:
-                # If the point is within the field, and does not contain zeros, add to filtered points
+            for field_name in points:
+                # If the point is in field, and does not contain zeros, add to field's list
                 if field_polygons[field_name].intersects(point) and not np.isnan(point.x) and not np.isnan(point.y):
-                    filtered_points[field_name].append({'latitude': row['lat'], 'longitude':row['long'], 'weight':row['weight']})
+                    points[field_name].append({'latitude': row['lat'], 'longitude':row['long'], 'weight':row['weight']})
         except:
             pass
-    return filtered_points
+    return points
 
 
 def quantize_yield_intensity(cfg, filtered_points):
-    yield_intensities = {}
 
+    def create_associated_region(initial_location, final_location):
+        # Calculating line vector
+        lx = final_location.x - initial_location.x
+        ly = final_location.y - initial_location.y
+
+        # Calculating normal vector to line traversed
+        ny = lx/ly
+        n_mag = np.sqrt(1 + ny**2)
+        ny = ny/n_mag
+        nx = 1/n_mag
+
+        # Create rectangle surrounding line
+        pool_height = cfg['pool_height']
+        region = Polygon([(initial_location.x + nx*pool_height, initial_location.y + ny*pool_height),
+                          (final_location.x + nx*pool_height, final_location.y + ny*pool_height),
+                          (final_location.x - nx*pool_height, final_location.y - ny*pool_height),
+                          (initial_location.x - nx*pool_height, initial_location.y - ny*pool_height)])
+
+        return geopandas.GeoSeries(region)
+
+    # Initialize yield intensities object with empty lists for each configured field
+    yield_intensities = {}
     for field in cfg['field_boundaries']:
         field_name = field['name']
         yield_intensities[field_name] = []
@@ -54,31 +75,19 @@ def quantize_yield_intensity(cfg, filtered_points):
         if len(filtered_points[field_name]) > 0:
             initial_point = filtered_points[field_name][0]
             for i in range(0, len(filtered_points[field_name])):
-                initial_point_series = geopandas.GeoSeries({'point': Point(float(initial_point['latitude']), float(initial_point['longitude']))})
-                i_point_series = geopandas.GeoSeries({'point': Point(float(filtered_points[field_name][i]['latitude']), float(filtered_points[field_name][i]['longitude']))})
-                distance = initial_point_series.distance(i_point_series)['point']
+                initial_location = Point(float(initial_point['latitude']), float(initial_point['longitude']))
+                i_location = Point(float(filtered_points[field_name][i]['latitude']), float(filtered_points[field_name][i]['longitude']))
+                distance = geopandas.GeoSeries({'point': initial_location}).distance(geopandas.GeoSeries({'point': i_location}))['point']
 
-                # Once distance traveled exceeds the configured distance interval, then yield intesity calculation happens
+                # Once distance traveled exceeds the configured distance interval, then calculate yield intensity
                 if distance > cfg['distance_interval']:
                     try:
                         # Removing anything that isn't a decimal point or a digit from recorded weights
                         i_weight = float(re.sub('[^A-Za-z0-9.]+', '', filtered_points[field_name][i]['weight']))
                         initial_weight = float(re.sub('[^A-Za-z0-9.]+', '', initial_point['weight']))
 
-                        # Calculating normal vector to line traversed
-                        lx = i_point_series['point'].x - initial_point_series['point'].x
-                        ly = i_point_series['point'].y - initial_point_series['point'].y
-                        ny = lx/ly
-                        n_mag = np.sqrt(1 + ny**2)
-                        ny = ny/n_mag
-                        nx = 1/n_mag
-                        pool_height = cfg['pool_height']
-                        region = Polygon([(initial_point_series['point'].x + nx*pool_height, initial_point_series['point'].y + ny*pool_height),
-                                          (i_point_series['point'].x + nx*pool_height, i_point_series['point'].y + ny*pool_height),
-                                          (i_point_series['point'].x - nx*pool_height, i_point_series['point'].y - ny*pool_height),
-                                          (initial_point_series['point'].x - nx*pool_height, initial_point_series['point'].y - ny*pool_height)])
                         yield_intensity = (i_weight - initial_weight)/ distance
-                        yield_intensities[field_name].append({'region': geopandas.GeoSeries(region), 'yield_intensity': yield_intensity})
+                        yield_intensities[field_name].append({'region': create_associated_region(initial_location, i_location), 'yield_intensity': yield_intensity})
 
                         # Reset initial point for next iteration
                         initial_point = filtered_points[field_name][i]
@@ -115,24 +124,6 @@ def remove_noise(processed_weight):
 # so maybe take a 400 ft square as the unit to include about 4 trees in 2 rows of 10 x 10 orchard to pool data.
 # Or, maybe we pool data from a radius around the point?
 
-def create_grid(cfg, field_polygons):
-    xmin,ymin,xmax,ymax = field_polygons.total_bounds
-
-    length = cfg['grid_interval']
-    width = cfg['grid_interval']
-
-    cols = list(np.arange(int(np.floor(xmin)), int(np.ceil(xmax)), width))
-    rows = list(np.arange(int(np.floor(ymin)), int(np.ceil(ymax)), length))
-    rows.reverse()
-
-    polygons = []
-    for x in cols:
-        for y in rows:
-            polygons.append( Polygon([(x,y), (x+width, y), (x+width, y-length), (x, y-length)]) )
-
-    grid = geopandas.GeoDataFrame({'geometry':polygons})
-    return grid
-
 
 def main():
     with open("config.json", 'r') as json_data_file:
@@ -141,8 +132,8 @@ def main():
         yield_intensities = []
         for filename in os.listdir(cfg['csv_directory']):
             if filename.lower().endswith(".csv"):
-                filtered_points = remove_points_not_in_fields(cfg, filename, field_polygons)
-                yield_intensities.append(quantize_yield_intensity(cfg, filtered_points))
+                data = load_file(cfg, filename, field_polygons)
+                yield_intensities.append(quantize_yield_intensity(cfg, data))
                 fig, ax = plt.subplots(1, 1)
                 for entry in yield_intensities[-1]['field_1']:
                     entry['region'].plot(ax=ax)
