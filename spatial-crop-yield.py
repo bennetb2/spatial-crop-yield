488cad64cc95c496069babf7ce0c47c72b01377d
Ian Wilson <wilsonia@oregonstate.edu>
2019-10-21 01:36:31 -0700

    add creation of polygons using normal vector

--- spatial-crop-yield.py
+++ spatial-crop-yield.py
@@ -6,112 +6,91 @@ import json
 import pandas as pd
 import geopandas
 import numpy as np
+import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon
 
 
 def create_field_polygons(cfg):
-    # Creating a polygon for each field in config
+    # Creating a GeoPandas polygon for each field in config
     field_corners = {}
     for field in cfg['field_boundaries']:
-        field_corners[field['name']] =  Polygon(
+        field_corners[field['name']] = Polygon(
                             [(field['NW_corner']['latitude'],field['NW_corner']['longitude']),
                             (field['NE_corner']['latitude'], field['NE_corner']['longitude']),
                             (field['SE_corner']['latitude'], field['SE_corner']['longitude']),
                             (field['SW_corner']['latitude'], field['SW_corner']['longitude'])])
-
-    # Geopandas library accounts for geometry of the polygons and land surfaces
     field_polygons = geopandas.GeoSeries(field_corners)
     return field_polygons
 
 
-# Step 1: SPATIALLY FILTER: Data filtered based on the field coordinates
-def filter_field_boundaries(cfg, filename, field_polygons):
+def remove_points_not_in_fields(cfg, filename, field_polygons):
     csv_directory = cfg['csv_directory']
     df = pd.read_csv(f'{csv_directory}/{filename}', encoding = "ISO-8859-1")
 
-    # Storing all points located within the field polygons
+    # Initialize lists for filtered points categorized by field name
     filtered_points = {}
     for field in cfg['field_boundaries']:
-        # Adding points to the filtered_points object within lists identified by field name
         filtered_points[field['name']] = []
 
     for index, row in df.iterrows():
         try:
-            # Iterating through rows of csv, creating a geopandas point using the df info
             point = Point(float(row['lat']), float(row['long']))
-
-            # Iterating through each named field w/in the filtered_points object
             for field_name in filtered_points:
-                # If the point is within the field, and does not contain zeros, add to that field
+                # If the point is within the field, and does not contain zeros, add to filtered points
                 if field_polygons[field_name].intersects(point) and not np.isnan(point.x) and not np.isnan(point.y):
-                    # Append statement creates an object including the lat/long and weight for included points
                     filtered_points[field_name].append({'latitude': row['lat'], 'longitude':row['long'], 'weight':row['weight']})
         except:
             pass
     return filtered_points
-    # filtered_points is now an object filled with lists of points within fields, and identified by field name
 
-# Step 2: PROCESS WEIGHT: Steps through data using a spatial increment to calculate change in weight
-def process_weight(cfg, filtered_points):
-    processed_weight = {}  # object set up like filtered_points, to store lists identified by field names
 
-    # Iterating through each field in the config
+def quantize_yield_intensity(cfg, filtered_points):
+    yield_intensities = {}
+
     for field in cfg['field_boundaries']:
-
         field_name = field['name']
+        yield_intensities[field_name] = []
 
-        # Storing the calculated "yields," or changes in weight, as lists under each field name
-        processed_weight[field_name] = []
-
-        # Setting an initial point to calculate distance for the delta weight calculation
-        try:
+        if len(filtered_points[field_name]) > 0:
             initial_point = filtered_points[field_name][0]
-        except:
-            pass
-        # Iterating through each point in the specific field the for loop above is at
-        for i in range(0, len(filtered_points[field_name])):
+            for i in range(0, len(filtered_points[field_name])):
+                initial_point_series = geopandas.GeoSeries({'point': Point(float(initial_point['latitude']), float(initial_point['longitude']))})
+                i_point_series = geopandas.GeoSeries({'point': Point(float(filtered_points[field_name][i]['latitude']), float(filtered_points[field_name][i]['longitude']))})
+                distance = initial_point_series.distance(i_point_series)['point']
 
-            # Defining the initial point and intermediate point as geopandas GeoSeries to allow for accurate distance calc
-            initial_point_series = geopandas.GeoSeries({'point': Point(float(initial_point['latitude']), float(initial_point['longitude']))})
-            i_point_series = geopandas.GeoSeries({'point': Point(float(filtered_points[field_name][i]['latitude']), float(filtered_points[field_name][i]['longitude']))})
-            # Calc distance between the intial point and intermediate point, allows for checking if distance interval is met yet
-            distance = initial_point_series.distance(i_point_series)['point']
+                # Once distance traveled exceeds the configured distance interval, then yield intesity calculation happens
+                if distance > cfg['distance_interval']:
+                    try:
+                        # Removing anything that isn't a decimal point or a digit from recorded weights
+                        i_weight = float(re.sub('[^A-Za-z0-9.]+', '', filtered_points[field_name][i]['weight']))
+                        initial_weight = float(re.sub('[^A-Za-z0-9.]+', '', initial_point['weight']))
 
-            # Once distance traveled exceeds the defined distance interval, then delta weight calculation happens
-            if distance > cfg['distance_interval']:
-                try:
-                    # Center point between initial point and intermediate point set up for delta weight
-                    midpoint = initial_point_series.union(i_point_series).centroid
+                        # Calculating normal vector to line traversed
+                        lx = i_point_series['point'].x - initial_point_series['point'].x
+                        ly = i_point_series['point'].y - initial_point_series['point'].y
+                        ny = lx/ly
+                        n_mag = np.sqrt(1 + ny**2)
+                        ny = ny/n_mag
+                        nx = 1/n_mag
+                        pool_height = cfg['pool_height']
+                        region = Polygon([(initial_point_series['point'].x + nx*pool_height, initial_point_series['point'].y + ny*pool_height),
+                                          (i_point_series['point'].x + nx*pool_height, i_point_series['point'].y + ny*pool_height),
+                                          (i_point_series['point'].x - nx*pool_height, i_point_series['point'].y - ny*pool_height),
+                                          (initial_point_series['point'].x - nx*pool_height, initial_point_series['point'].y - ny*pool_height)])
+                        yield_intensity = (i_weight - initial_weight)/ distance
+                        yield_intensities[field_name].append({'region': geopandas.GeoSeries(region), 'yield_intensity': yield_intensity})
 
-                    # Removing anything that isn't a decimal point or a digit from recorded weights
-                    i_weight = float(re.sub('[^A-Za-z0-9.]+', '', filtered_points[field_name][i]['weight']))
-                    initial_weight = float(re.sub('[^A-Za-z0-9.]+', '', initial_point['weight']))
+                        # Reset initial point for next iteration
+                        initial_point = filtered_points[field_name][i]
+                    except:
+                        pass
 
-                    # Calculating change in weight between initial point and next "step" in space
-                    delta_weight = i_weight - initial_weight
-
-                    # Saving the delta weight to field's list w/in processed_weight object IF the change in weight is reasonable
-                    # "Reasonable" currently defined as not extremely large or negative
-                    # if delta < 300 and delta > -500:
-
-                    # Append statement: appending objects containing lat/long/delta_weight to the field_name idenitified list
-                    processed_weight[field_name].append({'lat': midpoint.x['point'], 'long': midpoint.y['point'], 'delta_weight': delta_weight}) # not dividing by distance
-
-                    # Reset initial point for next run-through
-                    initial_point = filtered_points[field_name][i]
-                except:
-                    pass
-        # # Normalizing the delta weight by finding maximum value and dividing to create a percent/ratio
-        # max_weight = max([point['delta_weight'] for point in processed_weight[field_name]])
-        # for field_yield in processed_weight[field_name]:
-        #     field_yield['delta_weight'] = field_yield['delta_weight']/max_weight
-
-    return remove_noise(processed_weight)
+    return yield_intensities
 
 def remove_noise(processed_weight):
     # iterating over the field names w/in the processed_weight object
     for field_name in processed_weight:
-        # fetching the data (list of objects) for the field using the field name
+        # fetching the data (list of objects) for the field using the field namePolygon()
         field = processed_weight[field_name]
         noise = set()
         for i in range(0, len(field)):
@@ -155,19 +134,28 @@ def create_grid(cfg, field_polygons):
     return grid
 
 
-# Running all functions!
 def main():
     with open("config.json", 'r') as json_data_file:
         cfg = json.load(json_data_file)
         field_polygons = create_field_polygons(cfg)
-        # processed_weights = []
-        # for filename in os.listdir(cfg['csv_directory']):
-        #     if filename.endswith(".CSV"):
-        #         filtered_points = filter_field_boundaries(cfg, filename, field_polygons)
-        #         processed_weights.append(process_weight(cfg, filtered_points))
-        grid = create_grid(cfg, field_polygons)
-
+        yield_intensities = []
+        for filename in os.listdir(cfg['csv_directory']):
+            if filename.lower().endswith(".csv"):
+                filtered_points = remove_points_not_in_fields(cfg, filename, field_polygons)
+                yield_intensities.append(quantize_yield_intensity(cfg, filtered_points))
+                fig, ax = plt.subplots(1, 1)
+                for entry in yield_intensities[-1]['field_1']:
+                    entry['region'].plot(ax=ax)
+                plt.show()
+                # yield_intensities.append(remove_noise(yield_intensities))
+                # print(processed_weights[-1]['field_1'])
+                # output_df = pd.DataFrame(data=processed_weights[-1]['field_1'])
+                # output_df.to_csv(f'{filename}', index=False)
+        # grid = create_grid(cfg, field_polygons)
         # Output csv files with delta weight points for all fields
+
+        # output = pd.DataFrame(data=processed_weights)
+        # output.to_csv('output.csv', index=False)
     #    for field in output:
             #Output_df = pd.DataFrame(data=output[field])
             #Output_df.to_csv(f'{field}.csv', index=False)
